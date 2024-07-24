8119e7b991ce91f9dbc8018f8cb3720b89563e3b
Aarron Hulswitt <aarron.hulswitt@gmail.com>
2024-07-23 22:23:37 -0400

    removed Models from Api project

--- Server/Library.Api/Models/Book.cs
+++
@@ -1,9 +0,0 @@
-namespace Library.Api.Models;
-
-public class Book
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public string Description { get; set; }
-}
